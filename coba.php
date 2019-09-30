ce7adc4c21b97cfb5e7eb3c5ebb4647ddd4ee10d
Agung Alfarisy <agungalfarisy@gmail.com>
2019-09-30 17:39:21 +0700

    Menambahkan File coba.php

---
+++ coba.php
@@ -0,0 +1,21 @@
+<?php 
+
+clas Coba {
+	// public $a; // property
+
+	// // method
+	// public function b(){
+
+	// }
+}
+
+// membuat objek instance dari class
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+ ?>
